8b9532b01b2c22d08946c60643780f2d31991726
Marvin Liu <marvinliu9@gmail.com>
2015-10-10 10:46:10 -0700

    formatted info section

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 google.load('visualization', '1', {'packages': ['geochart']});
 $(document).ready(function() {
   google.setOnLoadCallback(drawMap);
-  
+  var chart;
+  var data;
+  var options;
   function drawMap() {
 
-    var data = new google.visualization.DataTable();
+    data = new google.visualization.DataTable();
 
     data.addColumn('number', 'latitude');                                
     data.addColumn('number', 'longitude');
-    data.addColumn('string', 'name'); 
+    data.addColumn('string', 'name');
+    data.addColumn('number', 'color'); 
     data.addColumn('number', 'value', 'value'); 
     data.addColumn({type:'string', role:'tooltip'});
     for (i = 1; i < 2; i++) {
@@ -21,34 +24,33 @@ $(document).ready(function() {
               geoCoord = geoCoord.split(" ");
               latitude = parseFloat(geoCoord[0]);
               longitude = parseFloat(geoCoord[1]);
-              data.addRows([[latitude, longitude, loan['name'], parseInt(loan['id']), loan['use']]]);
+              data.addRows([[latitude, longitude, loan['name'], 2, parseInt(loan['id']), loan['use']]]);
           });
         },
         async: false
       });
     }
 
-    var options = {
+    options = {
       displayMode: 'markers', 
       legend: 'none',
-      enableRegionInteractivity: 'true',
+      enableRegionInteractivity: 'false',
       backgroundColor: '#73CBFF',
       sizeAxis: {minSize:5,  maxSize: 5},
-      colorAxis: {minValue: 1, maxValue:1,  colors: ['#B92B3D']
+      colorAxis: {minValue: 1, maxValue:3,  colors: ['#00853f', '#e31b23']}
+    };
 
-    }
-  };
-
-  var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+  chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
 
   google.visualization.events.addListener(chart, 'select', function() {
       var selectionIdx = chart.getSelection()[0].row;
       var markerName = data.getValue(selectionIdx, 2);
-      var id = data.getValue(selectionIdx, 3);
+      var id = data.getValue(selectionIdx, 4);
       $.ajax({
         url:"http://api.kivaws.org/v1/loans/" + id.toString() + ".json",
         success: function(result) {
-          $(".info").empty();
+          $(".left-col").empty();
+          $(".right-col").empty();
           appendUserInfo(result);
         },
         async: false
@@ -57,11 +59,20 @@ $(document).ready(function() {
   
   chart.draw(data, options);
   };
+
   function appendUserInfo(result) {
     var loan = result['loans'][0];
-    $(".info").append("<div class=\"user-image\"><img src=\"http://www.kiva.org/img/w200h200/" + loan['image']['id'] + ".jpg\"></img></div>");
-    $(".info").append("<div class=\"name\">" + loan['name'] + "</div>");
-    $(".info").append("<div class=\"town-country\">" + loan['location']['town'] + ", " + loan['location']['country'] + "</div>");
-    $(".info").append("<div class=\"use\">" + loan['use'] + "</div>");
+
+    $(".left-col").append("<div class=\"user-image\"><img src=\"http://www.kiva.org/img/w200h200/" + loan['image']['id'] + ".jpg\"></img></div>");
+
+    $(".right-col").append("<div class=\"name\"><h1>" + loan['name'] + "</h1></div>");
+    $(".right-col").append("<div class=\"town-country\">" + loan['location']['town'] + ", " + loan['location']['country'] + "</div>");
+    $(".right-col").append("<div class=\"use\">" + loan['use'] + "</div>");
+
   }
+
+  window.onresize = function(event) {
+    chart.draw(data, options);
+  };
+
 });
