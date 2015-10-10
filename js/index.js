d98d6ee5992192a7d5ea1da7ec8283ec85cb45d4
Marvin Liu <marvinliu9@gmail.com>
2015-10-10 13:06:13 -0700

    added lender charts

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-google.load('visualization', '1', {'packages': ['geochart']});
+google.load('visualization', '1', {'packages': ['geochart', 'corechart']});
 $(document).ready(function() {
   google.setOnLoadCallback(drawMap);
   var chart;
   var data;
   var options;
+  document.getElementById("go").onclick = showLender;
   function drawMap() {
 
     data = new google.visualization.DataTable();
@@ -51,6 +52,7 @@ $(document).ready(function() {
         success: function(result) {
           $(".left-col").empty();
           $(".right-col").empty();
+          $(".visualizations").empty();
           appendUserInfo(result);
         },
         async: false
@@ -60,6 +62,20 @@ $(document).ready(function() {
   chart.draw(data, options);
   };
 
+  function showLender() {
+    var id = document.getElementById('id').value;
+    $.ajax({
+        url:"http://api.kivaws.org/v1/lenders/" + id.toString() + ".json",
+        success: function(result) {
+          var lender = result['lenders'][0];
+          $(".left-col").empty();
+          $(".right-col").empty();
+          appendLenderInfo(result);
+        },
+        async: false
+      });
+  }
+
   function appendUserInfo(result) {
     var loan = result['loans'][0];
 
@@ -67,8 +83,156 @@ $(document).ready(function() {
 
     $(".right-col").append("<div class=\"name\"><h1>" + loan['name'] + "</h1></div>");
     $(".right-col").append("<div class=\"town-country\">" + loan['location']['town'] + ", " + loan['location']['country'] + "</div>");
-    $(".right-col").append("<div class=\"use\">" + loan['use'] + "</div>");
+    $(".right-col").append("<div class=\"user-info-line\">" + loan['sector'] + "</div>");
+    $(".right-col").append("<div class=\"user-info-line\">$" + loan['funded_amount'] + "/$" + loan['loan_amount'] + " funded</div>");
+    $(".right-col").append("<div class=\"user-info-line\">" + loan['use'] + "</div>");
+    
+  }
 
+  function appendLenderInfo(result) {
+    var lender = result['lenders'][0];
+
+    $(".left-col").append("<div class=\"user-image\"><img src=\"http://www.kiva.org/img/w200h200/" + lender['image']['id'] + ".jpg\"></img></div>");
+
+    $(".right-col").append("<div class=\"name\"><h1>" + lender['name'] + "</h1></div>");
+    $(".right-col").append("<div class=\"town-country\">" + lender['whereabouts'] + "</div>");
+    $(".right-col").append("<div class=\"user-info-line\">Loans made: " + lender['loan_count'] + "</div>");
+    // $(".right-col").append("<div class=\"funding\">$" + loan['funded_amount'] + "/$" + loan['loan_amount'] + " funded</div>");
+    $(".right-col").append("<div class=\"user-info-line\">" + lender['occupational_info'] + "</div>");
+    var id = document.getElementById('id').value;
+    $.ajax({
+        url:"http://api.kivaws.org/v1/lenders/" + id.toString() + "/loans.json",
+        success: function(result) {
+          $(".visualizations").empty();
+          $(".visualizations").append("<div id=\"sectorpiechart\"></div>");
+          $(".visualizations").append("<div id=\"loanstatuspiechart\"></div>");
+          generateLenderGraphs(result);
+        },
+        async: false
+      });
+    
+  }
+
+  function generateLenderGraphs(result) {
+    var loans = result['loans'];
+    var sectorCount = [
+      ["Sector", "Number of Loans"],
+      ["Agriculture", 0],
+      ["Arts", 0],
+      ["Clothing", 0],
+      ["Construction", 0],
+      ["Education", 0],
+      ["Entertainment", 0],
+      ["Food", 0],
+      ["Health", 0],
+      ["Housing", 0],
+      ["Manufacturing", 0],
+      ["Personal Use", 0],
+      ["Retail", 0],
+      ["Services", 0],
+      ["Transportation", 0],
+      ["Wholesale", 0]
+    ];
+
+    var loanStatusCount = [
+      ["Loan Status", "Number of Loans"],
+      ["Fundraising", 0],
+      ["Funded", 0],
+      ["In Repayment", 0],
+      ["Paid", 0],
+      ["Defaulted", 0],
+      ["Refunded", 0]
+    ];
+
+    _.each(loans, function(loan) {
+      sector = loan['sector'];
+      status = loan['status'];
+      switch (sector) {
+        case "Agriculture":
+          sectorCount[1][1]++;
+          break;
+        case "Arts":
+          sectorCount[2][1]++;
+          break;
+        case "Clothing":
+          sectorCount[3][1]++;
+          break;
+        case "Construction":
+          sectorCount[4][1]++;
+          break;
+        case "Education":
+          sectorCount[5][1]++;
+          break;
+        case "Entertainment":
+          sectorCount[6][1]++;
+          break;
+        case "Food":
+          sectorCount[7][1]++;
+          break;
+        case "Health":
+          sectorCount[8][1]++;
+          break;
+        case "Housing":
+          sectorCount[9][1]++;
+          break;
+        case "Manufacturing":
+          sectorCount[10][1]++;
+          break;
+        case "Personal Use":
+          sectorCount[11][1]++;
+          break;
+        case "Retail":
+          sectorCount[12][1]++;
+          break;
+        case "Services":
+          sectorCount[13][1]++;
+          break;
+        case "Transportation":
+          sectorCount[14][1]++;
+          break;
+        case "Wholesale":
+          sectorCount[15][1]++;
+          break;
+        default:
+          console.log("sector doesn't match any found");
+          break;
+      } 
+
+      switch (status) {
+        case "fundraising":
+          loanStatusCount[1][1]++;
+          break;
+        case "funded":
+          loanStatusCount[2][1]++;
+          break;
+        case "in_repayment":
+          loanStatusCount[3][1]++;
+          break;
+        case "paid":
+          loanStatusCount[4][1]++;
+          break;
+        case "defaulted":
+          loanStatusCount[5][1]++;
+          break;
+        case "refunded":
+          loanStatusCount[6][1]++;
+          break;
+      }   
+    });
+
+    var sectorPieData = google.visualization.arrayToDataTable(sectorCount);
+    var sectorPieOptions = {
+      title: 'Loan Allocation'
+    };
+    var sectorPieChart = new google.visualization.PieChart(document.getElementById('sectorpiechart'));
+    sectorPieChart.draw(sectorPieData, sectorPieOptions);
+
+    var loanStatusData = google.visualization.arrayToDataTable(loanStatusCount);
+    var loanStatusOptions = {
+      title: 'Loan Status Breakdown'
+    };
+    var loanStatusChart = new google.visualization.PieChart(document.getElementById('loanstatuspiechart'));
+    loanStatusChart.draw(loanStatusData, loanStatusOptions);
   }
 
   window.onresize = function(event) {
