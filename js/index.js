59ca9de2b27aeddd6c61be92a3ebc840a8221f3c
Simon Cao <scsmncao@gmail.com>
2015-10-10 05:19:45 -0700

    added dynamic content

--- js/index.js
+++ js/index.js
@@ -1,53 +1,67 @@
-// $(document).ready(function() {
-//   $.getScript("https://www.google.com/jsapi", function() {
-//     console.log("got google visualization");
-//   });
-
-//   google.load('visualization', '1', {'packages': ['geochart']});
-//   google.setOnLoadCallback(drawMap);
+google.load('visualization', '1', {'packages': ['geochart']});
+$(document).ready(function() {
+  google.setOnLoadCallback(drawMap);
   
-//   function drawMap() {
+  function drawMap() {
 
-//     var data = new google.visualization.DataTable();
+    var data = new google.visualization.DataTable();
 
-//     data.addColumn('number', 'latitude');                                
-//     data.addColumn('number', 'longitude');
-//     data.addColumn('string', 'name'); 
-//     data.addColumn('number', 'value:', 'value'); 
-//     data.addColumn({type:'string', role:'tooltip'});
-//     $.ajax({
-//       url: "http://api.kivaws.org/v1/loans/newest.json",
-//       success: function(result) {
-//         var loanList = result['loans'];
-//         _.each(loanList, function(loan) {
-//             geoCoord = loan['geo']['pairs'];
-//             geoCoord = geoCoord.split(" ");
-//             latitude = parseInt(geoCoord[0]);
-//             longitude = parseInt(geoCoord[1]);
-//             data.addRows([[latitude, longitude, loan['name'], 0, loan['fundraising']]]);
-//         });
-//       }
-//     });
+    data.addColumn('number', 'latitude');                                
+    data.addColumn('number', 'longitude');
+    data.addColumn('string', 'name'); 
+    data.addColumn('number', 'value', 'value'); 
+    data.addColumn({type:'string', role:'tooltip'});
+    for (i = 1; i < 2; i++) {
+      $.ajax({
+        url: "http://api.kivaws.org/v1/loans/newest.json?page=" + i.toString(),
+        success: function(result) {
+          var loanList = result['loans'];
+          _.each(loanList, function(loan) {
+              geoCoord = loan['location']['geo']['pairs'];
+              geoCoord = geoCoord.split(" ");
+              latitude = parseFloat(geoCoord[0]);
+              longitude = parseFloat(geoCoord[1]);
+              data.addRows([[latitude, longitude, loan['name'], parseInt(loan['id']), loan['use']]]);
+          });
+        },
+        async: false
+      });
+    }
 
-//     var options = {
-//       displayMode: 'markers', 
-//       legend: 'none',
-//       enableRegionInteractivity: 'true',
+    var options = {
+      displayMode: 'markers', 
+      legend: 'none',
+      enableRegionInteractivity: 'true',
+      backgroundColor: '#73CBFF',
+      sizeAxis: {minSize:5,  maxSize: 5},
+      colorAxis: {minValue: 1, maxValue:1,  colors: ['#B92B3D']
 
-//       sizeAxis: {minSize:5,  maxSize: 5},
-//       colorAxis: {minValue: 1, maxValue:1,  colors: ['#B92B3D']
+    }
+  };
 
-//     }
-//   };
+  var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
 
-//   var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-
-//   google.visualization.events.addListener(chart, 'select', function() {
-//       var selectionIdx = chart.getSelection()[0].row;
-//       var markerName = data.getValue(selectionIdx, 2);
-//       console.log("you have selected " + markerName);
-//   });
+  google.visualization.events.addListener(chart, 'select', function() {
+      var selectionIdx = chart.getSelection()[0].row;
+      var markerName = data.getValue(selectionIdx, 2);
+      var id = data.getValue(selectionIdx, 3);
+      $.ajax({
+        url:"http://api.kivaws.org/v1/loans/" + id.toString() + ".json",
+        success: function(result) {
+          $(".info").empty();
+          appendUserInfo(result);
+        },
+        async: false
+      });
+  });
   
-//   chart.draw(data, options);
-//   };
-// });
+  chart.draw(data, options);
+  };
+  function appendUserInfo(result) {
+    var loan = result['loans'][0];
+    $(".info").append("<div class=\"user-image\"><img src=\"http://www.kiva.org/img/w200h200/" + loan['image']['id'] + ".jpg\"></img></div>");
+    $(".info").append("<div class=\"name\">" + loan['name'] + "</div>");
+    $(".info").append("<div class=\"town-country\">" + loan['location']['town'] + ", " + loan['location']['country'] + "</div>");
+    $(".info").append("<div class=\"use\">" + loan['use'] + "</div>");
+  }
+});
