8efcfebc06370a6ba08a3aa3095991b03b55a3bb
Marvin Liu <marvinliu9@gmail.com>
2015-10-11 00:50:02 -0700

    added force graph

--- js/forcegraph.js
+++ js/forcegraph.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
+  var w = 900;
+  var h = 400;
 
   var keyc = true, keys = true, keyt = true, keyr = true, keyx = true, keyd = true, keyl = true, keym = true, keyh = true, key1 = true, key2 = true, key3 = true, key0 = true
 
@@ -16,33 +16,26 @@ $(document).ready(function() {
     .domain([min_score, (min_score+max_score)/2, max_score])
     .range(["lime", "yellow", "red"]);
 
-  var highlight_color = "blue";
+  var highlight_color = "#cccccc";
   var highlight_trans = 0.1;
     
-  var size = d3.scale.pow().exponent(1)
-    .domain([1,100])
-    .range([8,24]);
-    
-  var force = d3.layout.force()
-    .linkDistance(60)
-    .charge(-300)
-    .size([w,h]);
+  var size = d3.scale.pow().exponent(1).domain([1,100]).range([8,24]);
+  var force = d3.layout.force().linkDistance(120).charge(-300).size([w,h]);
 
   var default_node_color = "#ccc";
   var default_link_color = "#888";
   var nominal_base_node_size = 8;
-  var nominal_text_size = 10;
+  var nominal_text_size = 12;
   var max_text_size = 24;
   var nominal_stroke = 1.5;
   var max_stroke = 4.5;
   var max_base_node_size = 36;
-  var min_zoom = 0.1;
-  var max_zoom = 7;
-  var svg = d3.select("body").append("svg");
+  var min_zoom = 3;
+  var max_zoom = 3;
+  var svg = d3.select(".forcegraph").append("svg");
   var zoom = d3.behavior.zoom().scaleExtent([min_zoom,max_zoom])
   var g = svg.append("g");
   svg.style("cursor","move");
-
 
   var graph = {};
 
@@ -84,18 +77,14 @@ $(document).ready(function() {
   else return default_link_color; })
 
 
-  var node = g.selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag)
+  var node = g.selectAll(".node").data(graph.nodes).enter().append("g").attr("class", "node").call(force.drag)
 
-  node.on("dblclick.zoom", function(d) { d3.event.stopPropagation();
-    var dcx = (window.innerWidth/2-d.x*zoom.scale());
-    var dcy = (window.innerHeight/2-d.y*zoom.scale());
-    zoom.translate([dcx,dcy]);
-    g.attr("transform", "translate("+ dcx + "," + dcy  + ")scale(" + zoom.scale() + ")");
-  });
+  // node.on("dblclick.zoom", function(d) { d3.event.stopPropagation();
+  //   var dcx = (window.innerWidth/2-d.x*zoom.scale());
+  //   var dcy = (window.innerHeight/2-d.y*zoom.scale());
+  //   zoom.translate([dcx,dcy]);
+  //   g.attr("transform", "translate("+ dcx + "," + dcy  + ")scale(" + zoom.scale() + ")");
+  // });
 
   var tocolor = "fill";
   var towhite = "stroke";
@@ -196,28 +185,28 @@ $(document).ready(function() {
     }
   }
 
-  zoom.on("zoom", function() {
-    var stroke = nominal_stroke;
-    if (nominal_stroke*zoom.scale()>max_stroke) stroke = max_stroke/zoom.scale();
-    link.style("stroke-width",stroke);
-    circle.style("stroke-width",stroke);
+  // zoom.on("zoom", function() {
+  //   var stroke = nominal_stroke;
+  //   if (nominal_stroke*zoom.scale()>max_stroke) stroke = max_stroke/zoom.scale();
+  //   link.style("stroke-width",stroke);
+  //   circle.style("stroke-width",stroke);
      
-    var base_radius = nominal_base_node_size;
-    if (nominal_base_node_size*zoom.scale()>max_base_node_size) base_radius = max_base_node_size/zoom.scale();
-    circle.attr("d", d3.svg.symbol()
-    .size(function(d) { return Math.PI*Math.pow(size(d.size)*base_radius/nominal_base_node_size||base_radius,2); })
-    .type(function(d) { return d.type; }))
+  //   var base_radius = nominal_base_node_size;
+  //   if (nominal_base_node_size*zoom.scale()>max_base_node_size) base_radius = max_base_node_size/zoom.scale();
+  //   circle.attr("d", d3.svg.symbol()
+  //   .size(function(d) { return Math.PI*Math.pow(size(d.size)*base_radius/nominal_base_node_size||base_radius,2); })
+  //   .type(function(d) { return d.type; }))
       
-    if (!text_center) text.attr("dx", function(d) { return (size(d.size)*base_radius/nominal_base_node_size||base_radius); });
+  //   if (!text_center) text.attr("dx", function(d) { return (size(d.size)*base_radius/nominal_base_node_size||base_radius); });
     
-    var text_size = nominal_text_size;
-    if (nominal_text_size*zoom.scale()>max_text_size) text_size = max_text_size/zoom.scale();
-    text.style("font-size",text_size + "px");
+  //   var text_size = nominal_text_size;
+  //   if (nominal_text_size*zoom.scale()>max_text_size) text_size = max_text_size/zoom.scale();
+  //   text.style("font-size",text_size + "px");
 
-    g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-  });
+  //   g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  // });
    
-  svg.call(zoom);   
+  // svg.call(zoom);   
 
   resize();
   d3.select(window).on("resize", resize).on("keydown", keydown);
@@ -235,7 +224,7 @@ $(document).ready(function() {
   });
 
   function resize() {
-    var width = window.innerWidth, height = window.innerHeight;
+    var width = 900, height = 400;
     svg.attr("width", width).attr("height", height);
     force.size([force.size()[0]+(width-w)/zoom.scale(),force.size()[1]+(height-h)/zoom.scale()]).resume();
     w = width;
